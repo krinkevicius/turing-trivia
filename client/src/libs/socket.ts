2371536c13b9de44e65cde7228d9f45a939dd060
Kristijonas <kristi.sr@hotmail.com>
2024-08-02 14:12:24 +0300

    Fixing prod build

--- client/src/libs/socket.ts
+++ client/src/libs/socket.ts
@@ -4,9 +4,9 @@ import type { ClientToServerEvents, ServerToClientEvents } from '@server/shared'
 import { USER_STORAGE_KEY } from '@/consts'
 
 // rkq: move or create env file
-// const SOCKET_PORT = 8181
+const SOCKET_PORT = 8181
 // const URL = `http://localhost:${SOCKET_PORT}`
-const URL = `https://turing-trivia-server.vercel.app`
+const URL = `https://turing-trivia-server.vercel.app:${SOCKET_PORT}`
 
 function getSessionID() {
   const userStorage = localStorage.getItem(USER_STORAGE_KEY)
