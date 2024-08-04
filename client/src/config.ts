750c3086fee7c34004ba58b7fd02eb91e255223e
Kristijonas <kristi.sr@hotmail.com>
2024-08-04 09:44:51 +0300

    Updated client config to pass tests

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,12 @@
 export const socketOrigin = import.meta.env.VITE_SOCKET_ORIGIN
 export const socketPort = import.meta.env.VITE_SOCKET_PORT
 export const sentryDSN = import.meta.env.VITE_SENTRY_CLIENT_DSN
+const isProduction = import.meta.env.VITE_ENVIRONMENT === 'production'
 
-if (typeof socketOrigin !== 'string') {
+if (isProduction && typeof socketOrigin !== 'string') {
   throw new Error('VITE_SOCKET_ORIGIN is not defined')
 }
 
-if (typeof socketPort !== 'string') {
+if (isProduction && typeof socketPort !== 'string') {
   throw new Error('VITE_SOCKET_PORT is not defined')
 }
