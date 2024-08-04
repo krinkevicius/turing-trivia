b76584b46aeaaf5020999ab115a6a31a5dce86b6
Kristijonas <kristi.sr@hotmail.com>
2024-08-04 15:28:01 +0300

    Updated style for phone screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="bg-bgPrimary text-textPrimary font-poppins">
       <div className="h-screen w-screen container mx-auto px-4">
         <Header />
-        <div className="h-[calc(100vh-150px)] max-h-screen">
+        <div className="h-[calc(100dvh-150px)] max-h-screen">
           <Body />
         </div>
       </div>
